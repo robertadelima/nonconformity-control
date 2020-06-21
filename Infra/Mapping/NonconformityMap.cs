e4c95c64a92838409903501487e89b6e0f8f38e6
robertadelima <robertarl@gmail.com>
2020-06-21 20:30:46 -0300

    code not required

--- Infra/Mapping/NonconformityMap.cs
+++ Infra/Mapping/NonconformityMap.cs
@@ -11,7 +11,7 @@ namespace NonconformityControl.Infra.Mapping
             builder.ToTable("Nonconformities");
             builder.HasKey(p => p.Id);
             builder.Property(p => p.Version);
-            builder.Property(p => p.Code).IsRequired().HasColumnType("VARCHAR(15)");
+            builder.Property(p => p.Code).HasColumnType("VARCHAR(15)");
             builder.Property(p => p.Description).HasMaxLength(1024).HasColumnType("VARCHAR(1024)");
             builder.Property(p => p.Status).HasColumnType("TINYINT");
             builder.Property(p => p.Evaluation).HasColumnType("TINYINT");
---
+++ Migrations/20200621232944_CodeNotRequired.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace NonconformityControl.Migrations
+{
+    public partial class CodeNotRequired : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Code",
+                table: "Nonconformities",
+                type: "VARCHAR(15)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "VARCHAR(15)");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Code",
+                table: "Nonconformities",
+                type: "VARCHAR(15)",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "VARCHAR(15)",
+                oldNullable: true);
+        }
+    }
+}
