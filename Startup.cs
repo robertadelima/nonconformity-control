e26b95cb05c25fa08c64256dee11cea6d5eb75a1
robertadelima <robertarl@gmail.com>
2020-06-21 21:34:33 -0300

    added swagger

--- Startup.cs
+++ Startup.cs
@@ -7,6 +7,7 @@ using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.OpenApi.Models;
 using NonconformityControl.Api.ViewModels;
 using NonconformityControl.Api.ViewModelsValidators;
 using NonconformityControl.Infra.Repositories;
@@ -34,11 +35,16 @@ namespace NonconformityControl
                         options.SerializerSettings.ReferenceLoopHandling = Newtonsoft.Json.ReferenceLoopHandling.Ignore
                     );
 
+             services.AddSwaggerGen(p => {
+                p.SwaggerDoc("v1", new OpenApiInfo{ Title= "Nonconformity Control API", Version = "v1"});
+            });
+
             services.AddScoped<NonconformityContext, NonconformityContext>();
             services.AddTransient<NonconformityRepository, NonconformityRepository>();
             services.AddTransient<ActionRepository, ActionRepository>();
 
             services.AddTransient<IValidator<AddNonconformityViewModel>, AddNonconformityViewModelValidator>();
+            services.AddTransient<IValidator<AddActionViewModel>, AddActionViewModelValidator>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -58,6 +64,12 @@ namespace NonconformityControl
             app.UseEndpoints(endpoints =>
             {
                 endpoints.MapControllers();
+            });
+
+            app.UseSwagger();
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "Nonconformity Control V1");
             });
         }
     }
